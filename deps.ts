e0824fd6163a96aa10e300a4dabe9d8a2738d06d
Justin Awrey <awreyjustin@gmail.com>
2022-10-07 15:09:12 -0700

    chore(deps): add parse-repo 1.0.4

--- deps.ts
+++ deps.ts
@@ -2,3 +2,4 @@ export * as colors from "https://deno.land/std@0.158.0/fmt/colors.ts";
 export * as semver from "https://deno.land/std@0.158.0/semver/mod.ts";
 export { Octokit } from "https://esm.sh/@octokit/core@4.0.5";
 export { restEndpointMethods } from "https://esm.sh/@octokit/plugin-rest-endpoint-methods@6.6.2";
+export { default as parseRepo } from "https://cdn.skypack.dev/parse-repo@1.0.4";
