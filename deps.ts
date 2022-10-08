a9a03d96581dc681bca33a58b969382d5d4a8640
Justin Awrey <awreyjustin@gmail.com>
2022-10-08 15:11:37 -0700

    chore(deps): use justinawrey/bash for invocating bash commands

--- deps.ts
+++ deps.ts
@@ -3,3 +3,4 @@ export * as semver from "https://deno.land/std@0.158.0/semver/mod.ts";
 export { Octokit } from "https://esm.sh/@octokit/core@4.0.5";
 export { restEndpointMethods } from "https://esm.sh/@octokit/plugin-rest-endpoint-methods@6.6.2";
 export { default as parseRepo } from "https://cdn.skypack.dev/parse-repo@1.0.4";
+export { bash } from "https://deno.land/x/bash@0.2.0/mod.ts";
--- git.ts
+++ git.ts
@@ -1,29 +1,6 @@
-import { colors, parseRepo } from "./deps.ts";
+import { bash, parseRepo } from "./deps.ts";
 
 export const ROOT = Symbol();
-
-async function bash(cmd: string): Promise<string> {
-  const process = Deno.run({
-    cmd: ["bash", "-c", cmd],
-    stdout: "piped",
-    stderr: "piped",
-  });
-
-  const [status, stdout, stderr] = await Promise.all([
-    process.status(),
-    process.output(),
-    process.stderrOutput(),
-  ]);
-
-  if (status.code === 0) {
-    return new TextDecoder().decode(stdout);
-  } else {
-    throw new Error(
-      `bash invocation "${colors.bold(cmd)}" failed with error:` +
-        new TextDecoder().decode(stderr),
-    );
-  }
-}
 
 interface LogOptions {
   grep: string;
@@ -33,7 +10,7 @@ interface LogOptions {
 export default {
   latestRemoteTag(): Promise<string> {
     return bash(
-      "git ls-remote --tags --sort=-v:refname origin | head -n 1 | cut -d/ -f3 | tr -d '\n'",
+      "git ls-remote --tags --sort=-v:refname origin | head -n 1 | cut -d/ -f3",
     );
   },
 
@@ -68,7 +45,7 @@ export default {
 
   async repoInfo(): Promise<{ repo: string; owner: string }> {
     const { project: repo, owner } = parseRepo(
-      await bash("git remote get-url origin | tr -d '\n'"),
+      await bash("git remote get-url origin"),
     );
 
     return {
